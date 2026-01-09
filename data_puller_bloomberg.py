e0d849937ee15673c37194f3f5dd309f5a3052c1
Sun <js3624@ic.ac.uk>
2026-01-09 14:37:54 +0000

    8

--- data_puller_bloomberg.py
+++ data_puller_bloomberg.py
@@ -3,12 +3,36 @@ import numpy as np
 from xbbg import blp
 
 #Portfolio construction
+print(blp.bdp("LLY US Equity", ["EQY_BETA", "EQY_BETA_BMK"]))
 
 stock_selection = pd.read_excel('stockselection.xlsx')
-tickers =  list(zip(
+# for types in list(set(stock_selection['Sec Type'])):
+stock_selection['api_ticker'] = np.where(
+    stock_selection['Sec Type'] = 'Equity',
+    stock_selection['Ticker'] +'Equity',
+    stock_selection['Ticker']
+)
+
+tickers =  list(set(stpcl)
     stock_selection['Ticker'], 
-    stock_selection['Weighting']
-    ))
+    stock_selection['Weighting'],
+    stock_selection['Sec Type']
+    )
+for tick in tickers:
+
 
 if tickers:
     print(f'Current porfolio composition is {tickers}')
+else: 
+    print('No underlying positions selected')
+
+
+
+#Pull prices from bloomberg
+print(blp.bdh(
+    tickers='AAPL US', 
+    flds=['High'],
+    start_date='2020-01-01',
+    end_date='today'
+              ))
+
